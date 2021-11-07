16ce2c4aa923f2a92270f8b5ad1aa349b0972909
firoze <firoze.hossain01@yahoo.com>
2021-11-07 20:53:40 +0600

    Bubble Sort Implemented with swaping

--- src/com/roze/bubblesort/BubbleSort.java
+++ src/com/roze/bubblesort/BubbleSort.java
@@ -2,6 +2,32 @@ package com.roze.bubblesort;
 
 public class BubbleSort {
     public static void main(String[] args) {
+int intAray[]={5,-9,56,66,8,2,-12};
+
+for (int lastUnsortedIndex=intAray.length-1;lastUnsortedIndex>0;lastUnsortedIndex--){
+    for (int i=0;i<lastUnsortedIndex;i++){
+        if (intAray[i]>intAray[i+1]){
+
+            swap(intAray,i,i+1);
+        }
+
+    }
+
+}
+
+for (int i=0;i<intAray.length;i++){
+    System.out.println(intAray[i]);
+}
+    }
+
+    public static void swap(int[] array,int i,int j){
+        if(i==j){
+            return;
+        }
+        int temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
+
 
     }
 }
