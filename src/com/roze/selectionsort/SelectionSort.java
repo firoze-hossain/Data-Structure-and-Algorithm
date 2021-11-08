347ea5db9bbe74a9331ec74aa0e70ed65358e214
firoze <firoze.hossain01@yahoo.com>
2021-11-08 13:03:38 +0600

    Selection Sort Implemented(formated)

--- src/com/roze/selectionsort/SelectionSort.java
+++ src/com/roze/selectionsort/SelectionSort.java
@@ -2,34 +2,34 @@ package com.roze.selectionsort;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] intArray={1,-98,6,78,3,-125,5};
+        int[] intArray = {1, -98, 6, 78, 3, -125, 5};
 
-        for(int lastUnsortedIndex=intArray.length-1;lastUnsortedIndex>0;lastUnsortedIndex--){
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
 
-                int largest=0;
-                for(int i=1;i<=lastUnsortedIndex;i++){
-                    if(intArray[i]>intArray[largest]){
-                        largest=i;
-                    }
+            int largest = 0;
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
                 }
-swap(intArray,largest,lastUnsortedIndex);
+            }
+            swap(intArray, largest, lastUnsortedIndex);
 
         }
-        for(int i=0;i<intArray.length;i++){
+        for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
 
         }
 
     }
 
-    public static void swap(int[] array,int i,int j){
-        if(i==j){
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
             return;
         }
 
-        int temp=array[i];
-        array[i]=array[j];
-        array[j]=temp;
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
 
     }
 }
