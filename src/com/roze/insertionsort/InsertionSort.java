e892f119845cd9073c6c729cd8b801a63fc9f70a
roze28 <firoze.hossain01@yahoo.com>
2021-11-11 18:50:49 +0600

    InsertionSort Implementation done (format)

--- src/com/roze/insertionsort/InsertionSort.java
+++ src/com/roze/insertionsort/InsertionSort.java
@@ -2,19 +2,19 @@ package com.roze.insertionsort;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int intArray[]={1,-89,21,63,7,8,9};
-        for(int firstUnsortedIndex=1;firstUnsortedIndex<intArray.length;firstUnsortedIndex++){
-            int newElement=intArray[firstUnsortedIndex];
+        int intArray[] = {1, -89, 21, 63, 7, 8, 9};
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
             int i;
-            for(i=firstUnsortedIndex;i>0 && intArray[i-1]>newElement;i--){
-                intArray[i]=intArray[i-1];
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
             }
-            intArray[i]=newElement;
+            intArray[i] = newElement;
 
         }
 
 
-        for(int i=0;i<intArray.length;i++){
+        for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
     }
