14177abe4d3bfdf4022a751157aa9a7649b3d515
roze28 <firoze.hossain01@yahoo.com>
2022-10-15 12:23:32 +0600

    two sum done

--- src/com/dsa/introduction/findTwoSum/FindTwoSum.java
+++ src/com/dsa/introduction/findTwoSum/FindTwoSum.java
@@ -1,4 +1,15 @@
 package com.dsa.introduction.findTwoSum;
 
+import java.util.Scanner;
+
 public class FindTwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Two Numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum of Two Numbers: " + sum);
+
+    }
 }
