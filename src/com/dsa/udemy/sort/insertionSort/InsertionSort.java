c9ac391fc3eddf2baafc5855845258f39a6d6161
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-06-18 23:45:02 +0600

    insertion sort

---
+++ src/com/dsa/udemy/sort/insertionSort/InsertionSort.java
@@ -0,0 +1,20 @@
+package com.dsa.udemy.sort.insertionSort;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = {12, -22, 71, -77, 2, 36};
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
+            int newElement = arr[firstUnsortedIndex];
+            int i;
+            for (i = firstUnsortedIndex; i > 0 && arr[i - 1] > newElement; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[i] = newElement;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
