cb165c70ada1a2d78d50e7e24371838936ac066e
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-06 15:07:04 +0600

    queue implementation using array

---
+++ src/com/dsa/udemy/queues/QueueUsingArray.java
@@ -0,0 +1,34 @@
+package com.dsa.udemy.queues;
+
+
+public class QueueUsingArray {
+    public static void main(String[] args) {
+        Employee firoze = new Employee("Md. Firoze", "Hossain", 111);
+        Employee rayhan = new Employee("Md. Sykhul", "Islam", 112);
+        Employee millat = new Employee("Md. Ahsanul Haque", "Millat", 113);
+        Employee imtiaze = new Employee("Ahammed", "Imtiaze", 114);
+        Employee mydul = new Employee("Md. Mydul", "Islam", 115);
+        ArrayQueue queue = new ArrayQueue(10);
+        queue.add(firoze);
+        queue.add(rayhan);
+        queue.add(millat);
+        queue.add(imtiaze);
+        queue.add(mydul);
+        queue.printQueue();
+        System.out.println("=========");
+        queue.remove();
+        queue.remove();
+        queue.printQueue();
+        System.out.println("=======");
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.printQueue();
+        System.out.println("=========");
+        queue.add(firoze);
+        queue.add(rayhan);
+        queue.printQueue();
+        System.out.println("======");
+        System.out.println(queue.peek());
+    }
+}
