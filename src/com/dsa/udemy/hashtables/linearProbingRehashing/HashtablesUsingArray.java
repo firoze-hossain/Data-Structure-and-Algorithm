5da9317d972847d14e26b55d625d1596ed5954e1
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-11 20:10:34 +0600

    linear probing rehashing

---
+++ src/com/dsa/udemy/hashtables/linearProbingRehashing/HashtablesUsingArray.java
@@ -0,0 +1,27 @@
+package com.dsa.udemy.hashtables.linearProbingRehashing;
+
+public class HashtablesUsingArray {
+    public static void main(String[] args) {
+        Employee firoze = new Employee("Md. Firoze", "Hossain", 111);
+        Employee rayhan = new Employee("Md. Sykhul", "Islam", 112);
+        Employee millat = new Employee("Md. Ahsanul Haque", "Millat", 113);
+        Employee imtiaze = new Employee("Ahammed", "Imtiaze", 114);
+        Employee mydul = new Employee("Md. Mydul", "Islam", 115);
+        SimpleHashtable hashtable = new SimpleHashtable();
+        hashtable.put("Hossain", firoze);
+        hashtable.put("Islam", rayhan);
+        hashtable.put("Millat", millat);
+        hashtable.put("Imtiaze", imtiaze);
+        hashtable.printHashtable();
+        System.out.println("==============");
+        System.out.println(hashtable.get("Millat"));
+        System.out.println(hashtable.get("Imtiaze"));
+        System.out.println("==========Removed==========");
+        hashtable.remove("Hossain");
+        hashtable.remove("Millat");
+        hashtable.printHashtable();
+        System.out.println("==========");
+        System.out.println(hashtable.get("Islam"));
+
+    }
+}
