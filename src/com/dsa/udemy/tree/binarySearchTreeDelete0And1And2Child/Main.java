a43ce7e3c9522e6a6631fad8f0f3e2c072bf52f4
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-20 21:16:17 +0600

    delete node with has two child implemented

---
+++ src/com/dsa/udemy/tree/binarySearchTreeDelete0And1And2Child/Main.java
@@ -0,0 +1,47 @@
+package com.dsa.udemy.tree.binarySearchTreeDelete0And1And2Child;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(15);
+        intTree.insert(27);
+        intTree.insert(30);
+        intTree.insert(29);
+        intTree.insert(26);
+        intTree.insert(22);
+        intTree.insert(32);
+
+        intTree.traverseInOrder();
+        System.out.println();
+//        System.out.println("===========");
+//        System.out.println(intTree.get(27));
+//        System.out.println(intTree.get(6666));
+//        System.out.println(intTree.get(20));
+//        System.out.println("==========");
+//        System.out.println(intTree.min());
+//        System.out.println(intTree.max());
+//        System.out.println("===========");
+//        intTree.delete(15);
+//        intTree.traverseInOrder();
+//        System.out.println();
+//
+//        System.out.println("===========");
+//        intTree.delete(17);
+//        intTree.traverseInOrder();
+//        System.out.println();
+
+//        System.out.println("===========");
+//        intTree.delete(25);
+//        intTree.traverseInOrder();
+//        System.out.println();
+
+        System.out.println("===========");
+        intTree.delete(2555);
+        intTree.traverseInOrder();
+        System.out.println();
+
+
+    }
+}
