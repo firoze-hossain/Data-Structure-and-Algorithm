046151b2198680769a588e561c32260cf606c872
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-23 15:30:24 +0600

    heaps insert done for max heaps

---
+++ src/com/dsa/udemy/heaps/heapsInsert/Heap.java
@@ -0,0 +1,35 @@
+package com.dsa.udemy.heaps.heapsInsert;
+
+public class Heap {
+    private int[] heap;
+    private int size;
+
+    public Heap(int capacity) {
+        heap = new int[capacity];
+    }
+
+    public boolean isFull() {
+        return size == heap.length;
+    }
+
+    public int getParent(int index) {
+        return (index - 1) / 2;
+    }
+
+    public void insert(int value) {
+        if (isFull()) {
+            throw new IndexOutOfBoundsException("Heap is full!!");
+        }
+        heap[size] = value;
+        fixHeapAbove(size);
+        size++;
+    }
+
+    public void fixHeapAbove(int index) {
+        int newValue = heap[index];
+        while (index > 0 && newValue > heap[getParent(index)]) {
+            heap[index] = heap[getParent(index)];
+        }
+        heap[index] = newValue;
+    }
+}
---
+++ src/com/dsa/udemy/heaps/heapsInsert/Main.java
@@ -0,0 +1,7 @@
+package com.dsa.udemy.heaps.heapsInsert;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
